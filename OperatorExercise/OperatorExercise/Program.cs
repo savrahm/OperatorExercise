301e0c5a1b8cd58a3e6c295a0f31072acf62825a
Sarah <sarah.b.avram@gmail.com>
2020-08-24 15:25:23 -0400

    pushing completed operator exercise

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var areaOfCircle = CalculateArea(radius);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}");
+        }
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+
+}
